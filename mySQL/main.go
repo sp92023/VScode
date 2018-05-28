c4011272637339b721efc81869513ac7f72a16d5
YiChun_Tsai <YiChun_Tsai@HTC.com>
2018-05-28 13:55:11 +0800

    modified:   mySQL/debug 	modified:   mySQL/main.go

--- mySQL/main.go
+++ mySQL/main.go
@@ -2,47 +2,118 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	db, err := sql.Open("mysql", "michael:michael@tcp(10.91.4.96:22)/YiChun?charset=utf8")
-	//db, err := sql.Open("mysql", "michael:michael@/YiChun?charset=utf8")
+	db, err := sql.Open("mysql", "michael:michael@tcp(10.91.4.96:3306)/YiChun?charset=utf8")
 
 	if err != nil {
 		log.Fatalf("error: %s", err)
 	}
 
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	/*var (
+	var (
 		ID       int
 		name     string
 		birthday string
 		gender   string
 	)
-	ID = 1041419
-	rows, err := db.Query("SELECT", ID)
+	/********************利用條件查詢(1)******************/
+	/*stmt, err := db.Prepare("select ID, name, birthday, gender from student where ID = ?")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer rows.Close()
+	//var name string
+	err = stmt.QueryRow(1041419).Scan(&ID, &name, &birthday, &gender)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(ID, name, birthday, gender)
+
+	err = stmt.QueryRow(1041108).Scan(&ID, &name, &birthday, &gender)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(ID, name, birthday, gender)*/
+	/**************************************/
+
+	/********************利用條件查詢(2)******************/
+	/*rows, err := db.Query("select name from student where gender=?", "female")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var count = 0
 	for rows.Next() {
-		if err := rows.Scan(&ID, &name, &birthday, &gender); err != nil {
+		err = rows.Scan(&name)
+		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("%d,%s,%s,%s", ID, name, birthday, gender)
+		fmt.Println(name)
+		count++
 	}
-	if err := rows.Err(); err != nil {
-		log.Fatal(err)
-	}*/
+	fmt.Printf("count female has %d\n", count)*/
+	/**************************************/
 
-	db.Exec("insert", 1041419, "Polly", "19960902", "female")
+	/*********************查詢全部資料*****************/
+	rows, err := db.Query("select * from student")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rows.Next() {
+		err = rows.Scan(&ID, &name, &birthday, &gender)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(ID, name, birthday, gender)
+	}
+	/**************************************/
+
+	/**********************加入資料****************/
+	/*insert, er := db.Prepare("insert into student value(?,?,?,?)")
+	if er != nil {
+		log.Fatal(er)
+	}
+	result, ers := insert.Exec(1040000, "Polly", "19960101", "female")
+	if ers != nil {
+		log.Fatal(ers)
+	}
+	rowCount, ers := result.RowsAffected()
+	if ers != nil {
+		log.Fatal(ers)
+	}
+	fmt.Printf("insert %d of data", rowCount)*/
+	/**************************************/
+
+	/**********************刪除資料****************/
+	/*del, errs := db.Prepare("delete from student where ID=?")
+	if errs != nil {
+		log.Fatal(errs)
+	}
+	res, errs := del.Exec(1040000)
+	if errs != nil {
+		log.Fatal(errs)
+	}
+	affect, errs := res.RowsAffected() //刪除幾筆資料
+	if errs != nil {
+		log.Fatal(errs)
+	}
+	fmt.Printf("delete %d of data", affect)*/
+	/**************************************/
+
+	/***********************資料更新***************/
+	/*update, errr := db.Prepare("update student set birthday=? where ID=?")
+	if errr != nil {
+		log.Fatal(errr)
+	}
+	_, errr = update.Exec("19960202", 1040000)
+	if errr != nil {
+		log.Fatal(errr)
+	}*/
+	/**************************************/
+
+	defer db.Close()
 }
